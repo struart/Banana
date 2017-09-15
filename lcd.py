e2de4826f592078b9925ff645f5e22515f85823e
Peter <struart88@gmail.com>
2017-09-15 20:18:00 +0200

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -7,10 +7,12 @@ import smbus
 import sqlite3 as lite
 
 #def main():
+I2C_LCD_driver.LCD_NOC = 1
 mylcd = I2C_LCD_driver.lcd()
 while True:
  mylcd.lcd_display_string("Godzina: %s" %time.strftime("%H:%M"), 2)
  mylcd.lcd_display_string("Data: %s" %time.strftime("%m/%d/%Y"), 3)
+# mylcd.backlight(1)
  time.sleep(20)
  try:
   con = lite.connect('/var/www/html/banan_baza.db')
@@ -27,28 +29,11 @@ while True:
  finally:
   if con:
    con.close()
-
-#sprawdz ktora godzina 
-
-
  mylcd.lcd_clear()
-
-#jesli nie noc to 
-# stan = 1 
-#else 
-# stan = 0
-#
-#mylcd.backlight(stan) 
  mylcd.lcd_display_string("Status modemu GSM", 1)
  mylcd.lcd_display_string("Siec: %s" %siec, 2)
  mylcd.lcd_display_string("Sygnal: -%s" %sygnal, 3)
  mylcd.lcd_display_string("ID: %s" %nadajnik, 4)
- 
+ #mylcd.backlight(1)
  time.sleep(20)
- 
- #jesli noc i stan == 1 
- # stan = 0
- #mylcd.backlight(stan)
- 
-
  mylcd.lcd_clear()
